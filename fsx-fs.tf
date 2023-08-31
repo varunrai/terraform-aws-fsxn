e54a568c323ffaecca888075bddbf5120e589701
Varun Seth <varun.seth@netapp.com>
2023-08-31 22:38:00 +0800

    updates

--- fsx-fs.tf
+++ fsx-fs.tf
@@ -8,7 +8,7 @@ resource "aws_fsx_ontap_file_system" "fsx_ontap_fs" {
   deployment_type     = var.filesystem.deployment_type_multiaz ? "MULTI_AZ_1" : "SINGLE_AZ_1"
   subnet_ids          = var.filesystem.subnet_ids
   preferred_subnet_id = var.filesystem.deployment_type_multiaz ? var.filesystem.preferred_subnet_id : var.filesystem.subnet_ids[0]
-  fsx_admin_password  = var.fsxn_password
+  fsx_admin_password  = var.filesystem_password
   security_group_ids  = var.filesystem.security_group_ids
 
   tags = {
