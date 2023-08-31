a316313a0722f72ad9312241c25797cfafb07324
Varun Seth <varun.seth@netapp.com>
2023-08-31 18:35:11 +0800

    updated the volume foreach variable

--- fsx-volume.tf
+++ fsx-volume.tf
@@ -2,16 +2,16 @@ resource "aws_fsx_ontap_volume" "fsxvol" {
 
   for_each = { for vol in var.volumes : vol.name => vol }
 
-  name                       = vol.value.name
-  junction_path              = vol.value.junction_path
-  security_style             = vol.value.security_style
-  size_in_megabytes          = vol.value.size_in_megabytes
-  storage_efficiency_enabled = vol.value.storage_efficiency_enabled
+  name                       = each.value.name
+  junction_path              = each.value.junction_path
+  security_style             = each.value.security_style
+  size_in_megabytes          = each.value.size_in_megabytes
+  storage_efficiency_enabled = each.value.storage_efficiency_enabled
   storage_virtual_machine_id = aws_fsx_ontap_storage_virtual_machine.fsxsvm[0].id
-  skip_final_backup          = vol.value.skip_final_backup
+  skip_final_backup          = each.value.skip_final_backup
 
   tiering_policy {
-    name           = vol.value.tiering_policy
-    cooling_period = (var.value.tiering_policy == "AUTO") ? vol.value.cooling_period : null
+    name           = each.value.tiering_policy
+    cooling_period = (each.value.tiering_policy == "AUTO") ? each.value.cooling_period : null
   }
 }
