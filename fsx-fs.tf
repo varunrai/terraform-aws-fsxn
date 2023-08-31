cada9d7ffd2bfc9e44b2167d00fad9f887f4bcf0
Varun Seth <varun.seth@netapp.com>
2023-08-31 18:27:09 +0800

    added the variable for fsxn size and throughput capacity

--- fsx-fs.tf
+++ fsx-fs.tf
@@ -3,8 +3,8 @@ locals {
 }
 
 resource "aws_fsx_ontap_file_system" "fsx_ontap_fs" {
-  storage_capacity    = 1024
-  throughput_capacity = var.default_throughput_capacity
+  storage_capacity    = var.fsxn_storage_capacity_in_gb
+  throughput_capacity = var.fsxn_throughput_capacity
   deployment_type     = var.deployment_type_multiaz ? "MULTI_AZ_1" : "SINGLE_AZ_1"
   subnet_ids          = var.fsxn_subnet_ids
   preferred_subnet_id = var.deployment_type_multiaz ? var.fsxn_preferred_subnet_id : var.fsxn_subnet_ids[0]
