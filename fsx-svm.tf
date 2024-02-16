f48b734ac91ed94d5bc71ec4b362cb46e89f723d
Varun Seth <varun.seth@netapp.com>
2024-02-17 07:34:00 +0800

    remove the lifecycle ignore since the activedirectory configuration bug was fixed in the provider.

--- fsx-svm.tf
+++ fsx-svm.tf
@@ -7,9 +7,9 @@ resource "aws_fsx_ontap_storage_virtual_machine" "fsxsvm" {
   root_volume_security_style = each.value.enable_smb ? "NTFS" : each.value.root_volume_security_style
   svm_admin_password         = each.value.svm_admin_password
 
-  lifecycle {
-    ignore_changes = [active_directory_configuration]
-  }
+  # lifecycle {
+  #   ignore_changes = [active_directory_configuration]
+  # }
 
   dynamic "active_directory_configuration" {
     for_each = each.value.enable_smb ? [1] : []
